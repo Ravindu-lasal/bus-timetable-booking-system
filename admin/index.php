a1063b3e5a2d65f6aa31f6fe289198f23049e259
Ravindu lasal <ravindusl01@gmail.com>
2024-11-02 12:11:19 +0530

    update

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,20 @@
 <?php include "include/header.php"; ?>
+<?php
+require_once 'include/inc.db_conn.php';
+// Query total students
+$usersquery = $pdo->query("SELECT COUNT(*) as total_users FROM users");
+$total_users = $usersquery->fetch(PDO::FETCH_ASSOC)['total_users'];
 
+$bookingsquery = $pdo->query("SELECT COUNT(*) as total_bookings FROM bookings");
+$total_bookings= $bookingsquery->fetch(PDO::FETCH_ASSOC)['total_bookings'];
+
+$usersbuses = $pdo->query("SELECT COUNT(*) as total_buses FROM buses");
+$total_buses = $usersbuses->fetch(PDO::FETCH_ASSOC)['total_buses'];
+
+$usersroutes = $pdo->query("SELECT COUNT(*) as total_routes FROM routes");
+$total_routes = $usersroutes->fetch(PDO::FETCH_ASSOC)['total_routes'];
+
+?>
     <main>
         <div class="container-fluid px-4">
             <h1 class="mt-4">Dashboard</h1>
@@ -14,7 +29,7 @@
                 <section id="minimal-statistics">
                     <div class="row">
                         <div class="col-12 mt-3 mb-1">
-                            <h4 class="text-uppercase">Minimal Statistics Cards</h4>
+                            <h4 class="text-uppercase">SLTB Bus Management</h4>
                         </div>
                     </div>
                     <div class="row mb-3 ">
@@ -27,8 +42,8 @@
                                                 <i class="icon-pencil primary font-large-2 float-left"></i>
                                             </div>
                                             <div class="media-body text-right">
-                                                <h3>278</h3>
-                                                <span>New Posts</span>
+                                                <h3><?= $total_users ?></h3>
+                                                <span>Useres</span>
                                             </div>
                                         </div>
                                     </div>
@@ -44,8 +59,8 @@
                                                 <i class="icon-speech warning font-large-2 float-left"></i>
                                             </div>
                                             <div class="media-body text-right">
-                                                <h3>156</h3>
-                                                <span>New Comments</span>
+                                                <h3><?= $total_bookings?></h3>
+                                                <span>Total Bookings</span>
                                             </div>
                                         </div>
                                     </div>
@@ -61,8 +76,8 @@
                                                 <i class="icon-graph success font-large-2 float-left"></i>
                                             </div>
                                             <div class="media-body text-right">
-                                                <h3>64.89 %</h3>
-                                                <span>Bounce Rate</span>
+                                                <h3><?= $total_buses?></h3>
+                                                <span>All buses</span>
                                             </div>
                                         </div>
                                     </div>
@@ -78,8 +93,8 @@
                                                 <i class="icon-pointer danger font-large-2 float-left"></i>
                                             </div>
                                             <div class="media-body text-right">
-                                                <h3>423</h3>
-                                                <span>Total Visits</span>
+                                                <h3><?= $total_routes?></h3>
+                                                <span>All Locations</span>
                                             </div>
                                         </div>
                                     </div>
